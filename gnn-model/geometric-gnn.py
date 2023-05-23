0c86f2428b2dd8b7001b5f89f5995d38f6db56e5
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2023-05-23 17:58:06 +0200

    feat: implemented simple classifier.

--- gnn-model/geometric-gnn.py
+++ gnn-model/geometric-gnn.py
@@ -1,42 +1,85 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
+from torch_geometric.nn import TransformerConv
+from torch_geometric.loader import DataLoader
+
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool
 
 from pathlib import Path
 from docopt import docopt
 
 from utils.datapreparation import prepareData
 
-class GCN(torch.nn.Module):
-    def __init__(self, data):
-        super().__init__()
-        self.conv1 = GCNConv(data.num_node_features, 16)
-        self.conv2 = GCNConv(16, 2)
+import random
+import sys
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-
-        return F.log_softmax(x, dim=1)
+class GNN(torch.nn.Module):
+    def __init__(self, input_size, hidden_channels):
+        super(GNN, self).__init__()
+        self.conv1 = GCNConv(
+            input_size, hidden_channels)
+        self.conv2 = GCNConv(
+            hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(
+            hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, 2)
     
-    def train(data, model, optimizer): 
+    def forward(self, x, edge_index, batch,  edge_attr):
+        x = self.conv1(x, edge_index, edge_attr)
+        x = x.relu()
+        x = self.conv2(x, edge_index, edge_attr)
+        x = x.relu()
+        x = self.conv3(x, edge_index, edge_attr)
+        
+        x = global_mean_pool(x, batch)
+        
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+    
+        return x
+    
+def train(data_loader, model, optimizer, criterion): 
+    model.train()
+    for data in data_loader:
         optimizer.zero_grad()
-        out = model(data)
-        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        out = model(data.x, data.edge_index, data.batch, data.edge_attr)
+        loss = criterion(out, data.y)
         loss.backward()
         optimizer.step()
-        return loss
+        
+def test(data_loader, model):
+    model.eval()
+    correct = 0
+    for data in data_loader:
+        out = model(data.x, data.edge_index, data.batch, data.edge_attr)  
+        pred = torch.argmax(out, dim=1)
+        #print(f'out: {F.softmax(out)}, pred: {pred}, y: {data.y}')
+        correct += int((pred == data.y).sum())
+    return correct / len(data_loader.dataset)
     
 if __name__ == "__main__":
-    # args = docopt(__doc__)
-    # dataset_dir = Path(args["ALL_DATA_FOLDER"])
-
-    dataset_dir = Path("C:\\Users\\Jurek\\git\\asdl-project-sander\\target")
-
+    dataset_dir = Path(sys.argv[1])
     
-    prepareData(dataset_dir)
-    
+    data_list = prepareData(dataset_dir)
+    random.shuffle(data_list)
+    data_list_train = data_list[:int(0.7*len(data_list))]
+    data_list_test = data_list[int(0.7*len(data_list)):]
+    print(f'Total Number of Graphs: {len(data_list)}, Number of Graphs for Training: {len(data_list_train)}, Number of Graphs for Tests: {len(data_list_test)}')
+
+    train_epoch = 200
+    batch_size = 32
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = GNN(1, 64).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    criterion = torch.nn.CrossEntropyLoss()
+    train_loader = DataLoader(data_list_train, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(data_list_test, batch_size=batch_size, shuffle=True)
+
+    for epoch in range (1, train_epoch+1):
+        train(train_loader, model, optimizer, criterion)
+        train_acc = test(train_loader, model)
+        test_acc = test(test_loader, model)
+        print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
--- utils/datapreparation.py
+++ utils/datapreparation.py
@@ -7,6 +7,8 @@ def prepareData(dataset_dir: RichPath):
     assert dataset_dir.exists()
 
     datalist = []
+    num_diff_nodes, num_diff_edge_attr = 0, 0
+    node_idx, edge_attr_idx = {}, {}
 
     for pkg_file in dataset_dir.rglob("*.msgpack.l.gz"):
         try:
@@ -15,11 +17,35 @@ def prepareData(dataset_dir: RichPath):
                     continue
                 nodes = []
                 for node in graph["graph"]["nodes"]:
-                    nodes.append(node)
-                #print(len(graph["graph"]["nodes"]))
-                # x = torch.tensor(nodes)
-                Data(x=nodes)
+                    if node in node_idx.keys():
+                        nodes.append([node_idx[node]])
+                    else:
+                        nodes.append([num_diff_nodes])
+                        node_idx[node] = num_diff_nodes
+                        num_diff_nodes += 1
                 
+                x = torch.tensor(nodes, dtype=torch.float)
+                outgoing_edges, incoming_edges, edge_attributes = [], [], []
+                for key in graph["graph"]["edges"].keys():
+                    for edge in graph["graph"]["edges"][key]:
+                        outgoing_edges.append(edge[0])
+                        incoming_edges.append(edge[1])
+                        if key in edge_attr_idx.keys():
+                            edge_attributes.append([edge_attr_idx[key]])
+                        else:
+                            edge_attributes.append([num_diff_edge_attr])
+                            edge_attr_idx[key] = num_diff_edge_attr
+                            num_diff_edge_attr += 1
+                edge_index = torch.tensor([outgoing_edges,
+                           incoming_edges], dtype=torch.long)
+                edge_attr = torch.tensor(edge_attributes, dtype=torch.float)
+                if graph["target_fix_action_idx"] is None:
+                    y_val = 0
+                else:
+                    y_val = 1
+                y = torch.tensor([y_val], dtype=torch.long)
+                data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+                datalist.append(data)
         except Exception as e:
             print(f"Error loading {pkg_file}: {e} Skipping...")
-    print('test')
+    return datalist
