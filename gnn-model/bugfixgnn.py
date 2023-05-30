c4bee9616d117e4d1195b663ce45149706182f0b
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2023-05-30 22:02:52 +0200

    feat: added ptgnn and vocabulary.

--- gnn-model/bugfixgnn.py
+++ gnn-model/bugfixgnn.py
@@ -1,18 +1,17 @@
 from typing_extensions import TypedDict
 from typing import Any, Counter, Dict, Iterator, List, NamedTuple, Optional, Tuple, Union
-
-# BuglabGraph = TypedDict(
-#     "BuglabGraph", 
-#     {
-#         "nodes": List[str],
-#         "edges": Dict[str, List[Union[Tuple[int, int], Tuple[int, int, str]]]],
-#         "path": str,
-#         "reference_nodes": List[int],
-#         "code_range": Tuple[Tuple[int, int], Tuple[int, int]]
-#     }
-# )
-
-# Prediction = Tuple[BuglabGraph, Dict[int, Tuple[str, float]]]
+from ptgnn.neuralmodels.gnn import (
+    GnnOutput,
+    GraphData,
+    GraphNeuralNetwork,
+    GraphNeuralNetworkModel,
+    TensorizedGraphData,
+)
+from ptgnn.baseneuralmodel import AbstractNeuralModel, ModuleWithMetrics
+import numpy as np
+from dpu_utils.mlutils import Vocabulary
+import re
+from dpu_utils.codeutils import split_identifier_into_parts
 
 class BugLabGraph(TypedDict):
     nodes: List[str]
@@ -30,3 +29,91 @@ class BugLabData(TypedDict):
     target_fix_action_idx: Optional[int]
     package_name: str
     candidate_rewrite_logprobs: Optional[List[float]]
+
+class BaseTensorizedBugLabGnn(NamedTuple):
+    graph_data: TensorizedGraphData
+
+    # Bug localization
+    target_location_node_idx: Optional[int]
+
+    # Bug repair
+    target_rewrites: List[int]
+    target_rewrite_to_location_group: List[int]
+    correct_rewrite_target: Optional[int]
+    text_rewrite_original_idx: List[int]
+
+    num_rewrite_locations_considered: int
+
+    # Bug selection
+    rewrite_logprobs: Optional[List[float]]
+
+class BugFixModule(ModuleWithMetrics):
+    pass
+
+IS_IDENTIFIER = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")
+
+class BugFixGnn(AbstractNeuralModel[BugLabData, BaseTensorizedBugLabGnn, BugFixModule]):
+    def __init__(
+        self,
+        gnn_model: GraphNeuralNetworkModel[str, Any],
+        max_num_classes: int = 100
+    ):
+        super().__init__()
+        self.__gnn_model = gnn_model
+        self.max_num_classes = max_num_classes
+
+    def __convert(self, buglab_data: BugLabData) -> Tuple[GraphData[str, None], Optional[int]]:
+        token_nodes = set()
+        if "NextToken" not in buglab_data["graph"]["edges"]:
+            return
+        for n1, n2 in buglab_data["graph"]["edges"]["NextToken"]:
+            token_nodes.add(n1)
+            token_nodes.add(n2)
+
+        vocab_nodes: Dict[str, int] = {}
+        vocab_edges: List[Tuple[int, int]] = []
+
+        all_nodes = buglab_data["graph"]["nodes"]
+        for node_idx in token_nodes:
+            token_str = all_nodes[node_idx]
+            if not IS_IDENTIFIER.match(token_str):
+                continue
+            for subtoken in split_identifier_into_parts(token_str):
+                subtoken_node_idx = vocab_nodes.get(subtoken)
+                if subtoken_node_idx is None:
+                    subtoken_node_idx = len(all_nodes)
+                    all_nodes.append(subtoken)
+                    vocab_nodes[subtoken] = subtoken_node_idx
+                vocab_edges.append((node_idx, subtoken_node_idx))
+
+        buglab_data["graph"]["edges"]["HasSubtoken"] = vocab_edges
+
+        edges = {}
+        edge_features = {}
+        for edge_type, adj_list in buglab_data["graph"]["edges"].items():
+            edges_of_type: List[Tuple[int, int]] = []
+            edge_features_of_type: List[str] = []
+            for edge in adj_list:
+                edges_of_type.append((edge[0], edge[1]))
+                if len(edge) >= 3:
+                    edge_features_of_type.append(edge[2])
+                else:
+                    edge_features_of_type.append(Vocabulary.get_pad())
+            edges[edge_type] = np.array(edges_of_type, dtype=np.int32)
+            edge_features[edge_type] = edge_features_of_type
+        
+        candidate_node_idxs, inv = np.unique(buglab_data["graph"]["reference_nodes"], return_inverse=True)
+        if buglab_data["target_fix_action_idx"] is not None:
+            target_node_idx = inv[buglab_data["target_fix_action_idx"]]
+        else:
+            target_node_idx = None
+        
+        return (GraphData[str](
+            node_information=buglab_data["graph"]["nodes"],
+            edges=edges,
+            edge_features=edge_features,
+            reference_nodes=candidate_node_idxs
+        ), target_node_idx)
+    
+    def initialize_metadata(self) -> None:
+        return super().initialize_metadata()
--- utils/datapreparation.py
+++ utils/datapreparation.py
@@ -5,6 +5,7 @@ import torch
 from torch_geometric.data import Data
 from typing import Any, Dict, List, NamedTuple, Optional, Tuple, Union
 import re
+from dpu_utils.mlutils import Vocabulary
 
 IS_IDENTIFIER = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")
 
@@ -13,7 +14,7 @@ def prepareData(dataset_dir: RichPath):
 
     datalist = []
     num_diff_nodes, num_diff_edge_attr = 0, 0
-    node_label_vocab, edge_attr_vocab = {}, {}
+    node_label_vocab, edge_attr_vocab = create_vocabs(dataset_dir)
 
     for pkg_file in dataset_dir.rglob("*.msgpack.l.gz"):
         try:
@@ -22,42 +23,37 @@ def prepareData(dataset_dir: RichPath):
                     continue
                 nodes = []
                 for node in graph["graph"]["nodes"]:
-                    if node in node_label_vocab.keys():
-                        nodes.append([node_label_vocab[node]])
-                    else:
-                        nodes.append([num_diff_nodes])
-                        node_label_vocab[node] = num_diff_nodes
-                        num_diff_nodes += 1
+                    nodes.append([node_label_vocab.get_id_or_unk(node)])
 
-                token_nodes = set()
-                if "NextToken" in graph["graph"]["edges"]:
-                    for n1, n2 in graph["graph"]["edges"]["NextToken"]:
-                        token_nodes.add(n1)
-                        token_nodes.add(n2)
+                # token_nodes = set()
+                # if "NextToken" in graph["graph"]["edges"]:
+                #     for n1, n2 in graph["graph"]["edges"]["NextToken"]:
+                #         token_nodes.add(n1)
+                #         token_nodes.add(n2)
 
-                    subtoken_idxs: Dict[int, int] = {}
-                    vocab_edges: List[Tuple[int, int]] = []
+                #     subtoken_idxs: Dict[int, int] = {}
+                #     vocab_edges: List[Tuple[int, int]] = []
 
-                    all_nodes = graph["graph"]["nodes"]
-                    for node_idx in token_nodes:
-                        token_str = all_nodes[node_idx]
-                        if not IS_IDENTIFIER.match(token_str):
-                            continue
-                        for subtoken in split_identifier_into_parts(token_str):
-                            subtoken_node_value = node_label_vocab.get(subtoken)
-                            if subtoken_node_value is None:
-                                subtoken_node_value = num_diff_nodes
-                                node_label_vocab[subtoken] = subtoken_node_value
-                                num_diff_nodes += 1
-                            subtoken_idx = subtoken_idxs.get(subtoken_node_value)
-                            if subtoken_idx is None:
-                                subtoken_idx = len(all_nodes)
-                                all_nodes.append(subtoken)
-                                nodes.append([subtoken_node_value])
-                                subtoken_idxs[subtoken_node_value] = subtoken_idx
-                            vocab_edges.append((node_idx, subtoken_idx))
+                #     all_nodes = graph["graph"]["nodes"]
+                #     for node_idx in token_nodes:
+                #         token_str = all_nodes[node_idx]
+                #         if not IS_IDENTIFIER.match(token_str):
+                #             continue
+                #         for subtoken in split_identifier_into_parts(token_str):
+                #             subtoken_node_value = node_label_vocab.get(subtoken)
+                #             if subtoken_node_value is None:
+                #                 subtoken_node_value = num_diff_nodes
+                #                 node_label_vocab[subtoken] = subtoken_node_value
+                #                 num_diff_nodes += 1
+                #             subtoken_idx = subtoken_idxs.get(subtoken_node_value)
+                #             if subtoken_idx is None:
+                #                 subtoken_idx = len(all_nodes)
+                #                 all_nodes.append(subtoken)
+                #                 nodes.append([subtoken_node_value])
+                #                 subtoken_idxs[subtoken_node_value] = subtoken_idx
+                #             vocab_edges.append((node_idx, subtoken_idx))
 
-                    graph["graph"]["edges"]["HasSubtoken"] = vocab_edges
+                #     graph["graph"]["edges"]["HasSubtoken"] = vocab_edges
                 
                 x = torch.tensor(nodes, dtype=torch.float)
                 outgoing_edges, incoming_edges, edge_attributes = [], [], []
@@ -65,12 +61,8 @@ def prepareData(dataset_dir: RichPath):
                     for edge in graph["graph"]["edges"][key]:
                         outgoing_edges.append(edge[0])
                         incoming_edges.append(edge[1])
-                        if key in edge_attr_vocab.keys():
-                            edge_attributes.append([edge_attr_vocab[key]])
-                        else:
-                            edge_attributes.append([num_diff_edge_attr])
-                            edge_attr_vocab[key] = num_diff_edge_attr
-                            num_diff_edge_attr += 1
+                        edge_attributes.append([edge_attr_vocab.get_id_or_unk(key)])
+                            
                 edge_index = torch.tensor([outgoing_edges,
                            incoming_edges], dtype=torch.long)
                 edge_attr = torch.tensor(edge_attributes, dtype=torch.float)
@@ -84,3 +76,23 @@ def prepareData(dataset_dir: RichPath):
         except Exception as e:
             print(f"Error loading {pkg_file}: {e} Skipping...")
     return datalist
+
+def create_vocabs(dataset_dir: RichPath) -> Tuple[Vocabulary, Vocabulary]:
+    node_labels, edge_types = set(), set()
+    for pkg_file in dataset_dir.rglob("*.msgpack.l.gz"):
+        try:
+            for graph in load_msgpack_l_gz(pkg_file):
+                if graph is None:
+                    continue
+                nodes = []
+                for node in graph["graph"]["nodes"]:
+                    node_labels.add(node)
+
+                for key in graph["graph"]["edges"].keys():
+                    edge_types.add(key)
+
+        except Exception as e:
+            print(f"Error loading {pkg_file}: {e} Skipping...")
+    node_vocab = Vocabulary.create_vocabulary(node_labels, max_size=300)
+    edge_type_vocab = Vocabulary.create_vocabulary(edge_types, max_size=300)
+    return node_vocab, edge_type_vocab
