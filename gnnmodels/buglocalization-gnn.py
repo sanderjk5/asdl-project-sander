cc5892262c0854a93f369b3ba1c61098dbced01a
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2023-06-12 18:07:54 +0200

    refactor: refactored top k feature.

--- gnnmodels/buglocalization-gnn.py
+++ gnnmodels/buglocalization-gnn.py
@@ -45,7 +45,6 @@ class GNN(torch.nn.Module):
         x = x.relu()
         x = self.conv2(x, data.edge_index, data.edge_attr)
         x = x.relu()
-        x = F.dropout(x, p= 0.2, training=self.training)
         x = self.conv3(x, data.edge_index, data.edge_attr)
         x = x.relu()
         x = F.dropout(x, p= 0.2, training=self.training)
@@ -53,7 +52,6 @@ class GNN(torch.nn.Module):
         x = x.relu()
         x = self.conv5(x, data.edge_index, data.edge_attr)
         x = x.relu()
-        x = F.dropout(x, p= 0.2, training=self.training)
         x = self.conv6(x, data.edge_index, data.edge_attr)
         x = x.relu()
         x = F.dropout(x, p= 0.2, training=self.training)
@@ -96,10 +94,9 @@ def train(data_loader, model, optimizer, criterion, useScaler, criterion_loc):
         running_loss += loss.item()
     return running_loss/len(data_loader)
         
-def test(data_loader, model, k):
+def test(data_loader, model, k_s):
     model.eval()
-    correct = 0
-    correct_topk = 0
+    correct_per_k = [0] * len(k_s)
     for data in data_loader:
         out = model(data)  
 
@@ -107,22 +104,20 @@ def test(data_loader, model, k):
         reference_y = torch.index_select(data.y, 0, reference_indices)
         y_index = (reference_y == 1).nonzero(as_tuple=True)[0].item()
         reference_out = F.softmax(torch.index_select(out, 0, reference_indices), dim=1)
-
-        pred_max_index = torch.argmax(reference_out, dim=0)[1]
-        if y_index == pred_max_index:
-            correct += 1
-
         num_ref_nodes = reference_out.size()[0]
-        if(num_ref_nodes >= k):
-            pred_max_indices = torch.topk(reference_out, k, dim=0)[1]
-        else:
-            pred_max_indices = torch.topk(reference_out, num_ref_nodes, dim=0)[1]
-        pred_max_indices = torch.transpose(pred_max_indices, 0, 1)[1]
-        # print(f'reference_out: {reference_out}, pred_max_indices: {pred_max_indices}, pred_max_indices[1]: {pred_max_indices[1]}, y_index: {y_index}, y_index in pred_max_indices: {y_index in pred_max_indices[1]}') 
-        if y_index in pred_max_indices:
-            correct_topk += 1
-        
-    return (correct / len(data_loader.dataset), correct_topk / len(data_loader.dataset))
+
+        for i in range(len(k_s)):   
+            k = k_s[i]     
+            if(num_ref_nodes >= k):
+                pred_max_indices = torch.topk(reference_out, k, dim=0)[1]
+            else:
+                pred_max_indices = torch.topk(reference_out, num_ref_nodes, dim=0)[1]
+            pred_max_indices = torch.transpose(pred_max_indices, 0, 1)[1]
+            # print(f'reference_out: {reference_out}, pred_max_indices: {pred_max_indices}, pred_max_indices[1]: {pred_max_indices[1]}, y_index: {y_index}, y_index in pred_max_indices: {y_index in pred_max_indices[1]}') 
+            if y_index in pred_max_indices:
+                correct_per_k[i] += 1
+
+    return [correct/len(data_loader) for correct in correct_per_k]
     
 if __name__ == "__main__":
     dataset_dir = Path(sys.argv[1])
@@ -130,55 +125,60 @@ if __name__ == "__main__":
     data_list, weights = prepareData(dataset_dir, True)
     random.shuffle(data_list)
     data_list_train = data_list[:int(0.7*len(data_list))]
-    data_list_test = data_list[int(0.7*len(data_list)):]
-    print(f'Total Number of Graphs: {len(data_list)}, Number of Graphs for Training: {len(data_list_train)}, Number of Graphs for Testing: {len(data_list_test)}') 
+    data_list_valid = data_list[int(0.7*len(data_list)):]
+    print(f'Total Number of Graphs: {len(data_list)}, Number of Graphs for Training: {len(data_list_train)}, Number of Graphs for Validation: {len(data_list_valid)}') 
 
-    train_epoch = 30
+    num_epochs = 50
     batch_size = 1
-    k = 3
+    k_s = [1, 3, 5]
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GNN(1, 256).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=5e-4)
-    # criterion = torch.nn.CrossEntropyLoss()
+    model = GNN(1, 128).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00005, weight_decay=5e-4)
     criterion = torch.nn.CrossEntropyLoss(weight=weights)
     criterion_loc = torch.nn.BCELoss()
     train_loader = DataLoader(data_list_train, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(data_list_test, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(data_list_valid, batch_size=batch_size, shuffle=True)
 
-    losses, train_accs, test_accs, train_accs_topk, test_accs_topk  = [], [], [], [], []
+    losses = []
+    train_accs, valid_accs = np.empty([len(k_s), num_epochs]), np.empty([len(k_s), num_epochs])
 
-    for epoch in range (1, train_epoch+1):
+    for epoch in range (1, num_epochs+1):
+        result = f'Epoch: {epoch:02d}'
         loss = train(train_loader, model, optimizer, criterion, False, criterion_loc)
         losses.append(loss)
-        train_acc = test(train_loader, model, k)
-        train_accs.append(train_acc[0])
-        train_accs_topk.append(train_acc[1])
-        test_acc = test(test_loader, model, k)
-        test_accs.append(test_acc[0])
-        test_accs_topk.append(test_acc[1])
-        print(f'Epoch: {epoch:03d}, Train Acc: {train_acc[0]:.4f}, Test Acc: {test_acc[0]:.4f}, Train Acc Top k: {train_acc[1]:.4f}, Test Acc Top k: {test_acc[1]:.4f}, Loss: {loss:.4f}')
+        train_acc = test(train_loader, model, k_s)
+        valid_acc = test(valid_loader, model, k_s)
+        for i in range(len(k_s)):
+            train_accs[i][epoch-1] = train_acc[i]
+            valid_accs[i][epoch-1] = valid_acc[i]
+            result += f', Train Acc Top {k_s[i]}: {train_acc[i]:.4f}'
+            result += f', Valid Acc Top {k_s[i]}: {valid_acc[i]:.4f}'
+        result += f', Loss: {loss:.4f}'
+        print(result)
 
+    x_values = [epoch for epoch in range (1, num_epochs+1)]
     plt.figure(0)
-    plt.plot(losses)
+    plt.plot(x_values, losses)
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.ylim((min(losses)-0.1, losses[5]+0.1))
     plt.savefig('losses_loc_detail.png')
 
     plt.figure(1)
-    plt.plot(losses)
+    plt.plot(x_values, losses)
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.savefig('losses_loc_total.png')
     
     plt.figure(2)
-    plt.plot(train_accs, label='Training')
-    plt.plot(test_accs, label='Testing')
-    plt.plot(train_accs_topk, label='Training Top k')
-    plt.plot(test_accs_topk, label='Testing Top k')
-    plt.legend(loc='lower right')
+    for i in range(len(k_s)):
+        label_training = 'Train Top ' + str(k_s[i])
+        label_valid = 'Valid Top ' + str(k_s[i])
+        plt.plot(x_values, train_accs[i], label=label_training)
+        plt.plot(x_values, valid_accs[i], label=label_valid)
+    plt.legend(loc='upper left')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
-    plt.ylim((0, max(max(train_accs_topk), max(test_accs_topk))+0.05))
+    plt.ylim((0, max(max(train_accs[len(k_s)-1]), max(valid_accs[len(k_s)-1]))+0.05))
     plt.savefig('accuracies_loc.png')
     plt.close()
